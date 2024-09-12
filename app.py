a2f97ca5b2ffb12ad33a89fa1945aedd94ab2cee
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2024-09-12 14:18:02 -0700

    ez mode done

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story 
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password123'
+
+@app.route('/')
+def home():
+
+    prompts = story.prompts
+
+    return render_template('madlibs.html', prompts=prompts)
+
+@app.route('/story')
+def show_story():
+    
+    text = story.generate(request.args)
+
+    return render_template('story.html', text=text)
+
+
+
+
